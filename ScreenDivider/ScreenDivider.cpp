26eb01e55b7e9d5f927aef543a55ddaa1d2b511b
Jun-ho, Song <jun9358@gmail.com>
2013-11-16 00:41:27 +0900

    Set test environment
    To test setting dialogs, add showing code that show setting_style and
    setting_general dialog.

--- ScreenDivider/ScreenDivider.cpp
+++ ScreenDivider/ScreenDivider.cpp
@@ -111,70 +111,13 @@ BOOL CScreenDividerApp::InitInstance()
 	// such as the name of your company or organization
 	SetRegistryKey(_T("Local AppWizard-Generated Applications"));
 
-	/*
-	CAeroDlg dlg;
-	m_pMainWnd = &dlg;
-	INT_PTR nResponse = dlg.DoModal();
-	if (nResponse == IDOK)
-	{
-		// TODO: Place code here to handle when the dialog is
-		//  dismissed with OK
-	}
-	else if (nResponse == IDCANCEL)
-	{
-		// TODO: Place code here to handle when the dialog is
-		//  dismissed with Cancel
-	}
+	CSettingStyleDlg dlgSettingStyle;
+	dlgSettingStyle.Create(IDD_SETTING_STYLE_DIALOG);
+	dlgSettingStyle.ShowWindow(TRUE);
 
-	// Delete the shell manager created above.
-	if (pShellManager != NULL)
-	{
-		delete pShellManager;
-	}
-	*/
-	/*
-	CEditorDlg dlg;
-	m_pMainWnd = &dlg;
-	INT_PTR nResponse = dlg.DoModal();
-	if (nResponse == IDOK)
-	{
-		// TODO: Place code here to handle when the dialog is
-		//  dismissed with OK
-	}
-	else if (nResponse == IDCANCEL)
-	{
-		// TODO: Place code here to handle when the dialog is
-		//  dismissed with Cancel
-	}
-	*/
-	/*
-	CFinderDlg dlg;
-	m_pMainWnd = &dlg;
-	INT_PTR nResponse = dlg.DoModal();
-	if (nResponse == IDOK)
-	{
-		// TODO: Place code here to handle when the dialog is
-		//  dismissed with OK
-	}
-	else if (nResponse == IDCANCEL)
-	{
-		// TODO: Place code here to handle when the dialog is
-		//  dismissed with Cancel
-	}
-
-	// Delete the shell manager created above.
-	if (pShellManager != NULL)
-	{
-		delete pShellManager;
-	}
-	*/
-	CSettingStyleDlg dlg2;
-	dlg2.Create(IDD_SETTING_STYLE_DIALOG);
-	dlg2.ShowWindow(TRUE);
-
-	CSettingGeneralDlg dlg;
-	m_pMainWnd = &dlg;
-	INT_PTR nResponse = dlg.DoModal();
+	CSettingGeneralDlg dlgSettingGeneral;
+	m_pMainWnd = &dlgSettingGeneral;
+	INT_PTR nResponse = dlgSettingGeneral.DoModal();
 	if (nResponse == IDOK)
 	{
 		// TODO: Place code here to handle when the dialog is
